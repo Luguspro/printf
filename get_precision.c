58982bce49b473485f2cff8df55ff717aacc0928
Serugo Akram <58663762+Alleram@users.noreply.github.com>
2023-08-26 18:28:29 +0300

    Update get_precision.c

--- get_precision.c
+++ get_precision.c
@@ -8,10 +8,11 @@
 *
 * Return: Precision
 */
+
 int get_precision(const char *format, int *i, va_list list)
 {
-	int curr_i = *i + 1;
-	int precision = -1;
+int curr_i = *i + 1;
+int precision = -1;
 
 if (format[curr_i] != '.')
 	return (precision);

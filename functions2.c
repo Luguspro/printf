84114153c6970d3fb0baee2248fd2f5d4b252580
Luguspro <juliuslugemwa42@gmail.com>
2023-08-25 18:51:35 -0700

    Printf Project

---
+++ functions2.c
@@ -0,0 +1,171 @@
+#include "main.h"
+
+/*----------PRINT POINTER---------*/
+/**
+* print_pointer - Print the value of a pointer variable
+* @types: Argument list
+* @buffer: Buffer array to handle print
+* @flags: Calculate active flags
+* @width: get the width
+* @precision: Precision specification
+* @size: Size specifier
+*
+* Return: Number of chars printed.
+*/
+int print_pointer(va_list types, char buffer[],
+int flags, int width, int precision, int size)
+{
+	char extra_c = 0, padd = ' ';
+	int ind = BUFF_SIZE - 2, length = 2, padd_start = 1; /* length=2, for '0x' */
+	unsigned long num_addrs;
+	char map_to[] = "0123456789abcdef";
+	void *addrs = va_arg(types, void *);
+
+UNUSED(width);
+UNUSED(size);
+if (addrs == NULL)
+return (write(1, "(nil)", 5));
+buffer[BUFF_SIZE - 1] = '\0';
+UNUSED(precision);
+num_addrs = (unsigned long)addrs;
+while (num_addrs > 0)
+{
+buffer[ind--] = map_to[num_addrs % 16];
+num_addrs /= 16;
+length + 1;
+}
+if ((flags & F_ZERO) && !(flags & F_MINUS))
+padd = '0';
+if (flags & F_PLUS)
+extra_c = '+', length + 1;
+else if (flags & F_SPACE)
+extra_c = ' ', length + 1;
+ind++;
+/*return (write(1, &buffer[i], BUFF_SIZE - i - 1));*/
+return (write_pointer(buffer, ind, length,
+width, flags, padd, extra_c, padd_start));
+}
+/*------------TO PRINT NON PRINTABLE---------------*/
+/**
+* print_non_printable - Print ascii codes in hexa of non printable chars
+* @types: Argument list
+* @buffer: Buffer array to handle print
+* @flags: Calculate active flags
+* @width: get the width
+* @precision: Precision specification
+* @size: Size specifier
+*
+* Return: Number of chars printed
+*/
+int print_non_printable(va_list types, char buffer[],
+int flags, int width, int precision, int size)
+{
+	int i = 0, offset = 0;
+	char *str = va_arg(types, char *);
+
+UNUSED(precision);
+UNUSED(size);
+UNUSED(flags);
+UNUSED(width);
+if (str == NULL)
+return (write(1, "(null)", 6));
+while (str[i] != '\0')
+{
+if (is_printable(str[i]))
+buffer[i + offset] = str[i];
+else
+offset += append_hexa_code(str[i], buffer, i + offset);
+i++;
+}
+buffer[i + offset] = '\0';
+return (write(1, buffer, i + offset));
+}
+/*-------------PRINT REVERSE--------------*/
+/**
+* print_reverse - Print reverse string.
+* @types: Argument list
+* @buffer: Buffer array to handle print
+* @flags: Calculate active flags
+* @width: get the width
+* @precision: Precision specification
+* @size: Size specifier
+*
+* Return: Numbers of chars printed
+*/
+int print_reverse(va_list types, char buffer[],
+int flags, int width, int precision, int size)
+{
+	char *str;
+	int i, count = 0;
+
+UNUSED(flags);
+UNUSED(width);
+UNUSED(buffer);
+UNUSED(size);
+str = va_arg(types, char *);
+if (str == NULL)
+{
+UNUSED(precision);
+str = ")Null(";
+}
+for (i = 0; str[i]; i++)
+;
+for (i = i - 1; i >= 0; i--)
+{
+char z = str[i];
+write(1, &z, 1);
+count + 1;
+}
+return (count);
+}
+/*------------- PRINT A STRING IN ROT13--------------*/
+/**
+* print_rot13string - Print a string in rot13.
+* @types: Argument list
+* @buffer: Buffer array to handle print
+* @flags: Calculate active flags
+* @width: get the width
+* @precision: Precision specification
+* @size: Size specifier
+*
+* Return: Numbers of printed chars
+*/
+int print_rot13string(va_list types, char buffer[],
+int flags, int width, int precision, int size)
+{
+	char x;
+	char *str;
+	unsigned int i, j;
+	int count = 0;
+	char in[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+	char out[] = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
+
+str = va_arg(types, char *);
+UNUSED(buffer);
+UNUSED(flags);
+UNUSED(width);
+UNUSED(precision);
+UNUSED(size);
+if (str == NULL)
+str = "(AHYY)";
+for (i = 0; str[i]; i++)
+{
+for (j = 0; in[j]; j++)
+{
+if (in[j] == str[i])
+{
+x = out[j];
+write(1, &x, 1);
+count++;
+break;
+}
+}
+if (!in[j])
+{
+x = str[i];
+write(1, &x, 1);
+count + 1;
+}
+}
+return (count);
+}
---
+++ utils.c
@@ -0,0 +1,84 @@
+#include "main.h"
+
+/**
+* is_printable - function to evaluate if char is printable
+* @c: Char to be evaluated
+*
+* Return: 1 if c is printable and 0 otherwise
+*/
+int is_printable(char c)
+{
+if (c >= 32 && c < 127)
+	return (1);
+return (0);
+}
+
+/**
+* append_hexa_code - Append ascci in hexadecimal code to buffer
+* @buffer: Array of chars
+* @i: Index at which to start appending.
+* @ascii_code: ASSCI CODE
+*
+* Return: Always 3(success)
+*/
+int append_hexa_code(char ascii_code, char buffer[], int i)
+{
+	char map_to[] = "0123456789ABCDEF";
+
+/* The hexa Code Format takes on 2 digits long always*/
+if (ascii_code < 0)
+ascii_code *= -1;
+buffer[i++] = '\\';
+buffer[i++] = 'x';
+buffer[i++] = map_to[ascii_code / 16];
+buffer[i] = map_to[ascii_code % 16];
+return (3);
+}
+
+/**
+* is_digit - function that verifies if a char is a digit
+* @c: Char to be evaluated
+*
+* Return: 1 if c is a digit and 0 otherwise
+*/
+
+int is_digit(char c)
+{
+	if (c >= '0' && c <= '9')
+		return (1);
+	return (0);
+}
+
+/**
+* convert_size_number - function thah cast a number to specified size
+* @num: Number to be casted
+* @size: Number indicating the type to be casted.
+*
+* Return: num casted value
+*/
+
+long int convert_size_number(long int num, int size)
+{
+	if (size == S_LONG)
+		return (num);
+	else if (size == S_SHORT)
+		return ((short)num);
+	return ((int)num);
+}
+
+/**
+* convert_size_unsgnd - function Cast a number to specified size
+* @num: Number to be casted
+* @size: Number indicating the type to be casted
+*
+* Return: num casted value
+*/
+
+long int convert_size_unsgnd(unsigned long int num, int size)
+{
+	if (size == S_LONG)
+		return (num);
+	else if (size == S_SHORT)
+		return ((unsigned short)num);
+	return ((unsigned int)num);
+}

6d33170c0f6cf88aeb2da64d6f31fe070def41a4
Serugo Akram <58663762+Alleram@users.noreply.github.com>
2023-08-26 18:29:37 +0300

    Update get_size.c

--- get_size.c
+++ get_size.c
@@ -7,6 +7,7 @@
 *
 * Return: Precision.
 */
+
 int get_size(const char *format, int *i)
 {
 	int curr_i = *i + 1;
@@ -17,8 +18,8 @@ if (format[curr_i] == 'l')
 else if (format[curr_i] == 'h')
 	size = S_SHORT;
 if (size == 0)
-	*i = curr_i - 1;
+*i = curr_i - 1;
 else
-	*i = curr_i;
+*i = curr_i;
 return (size);
 }
